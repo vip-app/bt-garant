e00e67945cad40bfdfac9e3b1e3ec7e19f8f8f3a
vip-app <seemenu.ru@gmail.com>
2023-04-27 17:14:36 +0300

    171427042023

---
+++ resources/shop-data.js
@@ -0,0 +1,15 @@
+const data = {
+		title: "BTGarant",
+		tel: "79640626525",
+		whatsapp: "79640626525",
+		telegram: "",
+		image: "",
+		email: "seemenu.ru@gmail.com",
+		location: "43.668874, 45.101263",
+		address: {
+			street: "На территории заправки Groztek",
+			city: "Знаменское",
+			region: "Надтеречный район",
+			country: "ЧР"
+		}
+	};
